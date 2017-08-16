bec671e4b8a972262b87d6201b0aa26e14bfb9bf
Kevin <Kevin@Roselynes-MacBook-Pro.local>
2017-08-16 11:54:34 +0200

    statistics

---
+++ public/Statistics/crimeRelated.php
@@ -0,0 +1,175 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+      
+      <!-- Main Css--> 
+      <link rel="stylesheet" type="text/css" href="crimeRelated.css?version=50">
+      
+      <!-- Font awesom link -->
+      <script src="https://use.fontawesome.com/5149f9eb72.js"></script>
+      
+      <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Playfair Display" rel="stylesheet">
+      
+      
+  </head>
+    
+    
+    
+    
+    
+  <body>
+      
+      <!-- Navbar -->
+      
+    
+        <nav class="navbar fixed-top navbar-toggleable-sm navbar-inverse bg-inverse">
+        
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        
+  <a class="navbar-brand" href="#"><img style="width:30px; height:30px;" src="sheriff.png"></a>
+  <div class="collapse navbar-collapse  justify-content-end" id="navbarNavAltMarkup">
+    <ul class="navbar-nav ">
+        
+      <li class="nav-item">
+        <a class="nav-link" href="index2.html">Home</a>
+      </li>
+        
+        
+      <li class="nav-item">
+        <a class="nav-link" href="#">Login</a>
+      </li>
+        
+        
+      <li class="nav-item">
+        <a class="nav-link" href="#">Reports</a>
+      </li>
+        
+        
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Statistics<span class="sr-only">(current)</span></a>
+      </li>
+        
+        
+        <li class="nav-item">
+        <a class="nav-link" href="#">Hotspots</a>
+      </li>
+        
+        
+        
+        
+        <li class="nav-item">
+        <a class="nav-link" href="#">Contact us</a>
+      </li>
+        
+    </ul>
+            </div>
+
+</nav>
+        
+       
+    
+    
+            
+       
+       <div id="mainSection">
+           
+           <div class="container">
+               
+               <h1 style="text-align:center;">Related Crimes</h1>
+              
+                   <img src="map.png">
+               
+              
+           
+           </div>
+           
+       
+           
+    </div>
+            
+            
+            
+            
+            
+                
+                
+                  <!--footer -->
+            
+            <div id="footer">
+                
+                <div class="row">
+                
+                    <div class="col-lg-6 col-md-6 col-sm-6">
+                        
+                        <h5>Head Office Address</h5>
+                        <p class="Footerinfo">11 Imam Haron Road<br>Claremont<br>Cape-Town  <br>7708<br>South Africa<br>+27 (0)711-711-711</p>
+                      
+                       
+                    
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-sm-6">
+                        
+                        <h5 style="float:right; padding-right:20px !important;"> Where we stay <br> <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3308.3746314959844!2d18.467969950155833!3d-33.982909880529675!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1dcc42d744c835bf%3A0xd27135616b9ed72!2sBrookside+Office+Park%2C+11+Imam+Haron+Rd%2C+Claremont%2C+Cape+Town%2C+7708!5e0!3m2!1sen!2sza!4v1502104444977" width="200" height="117" frameborder="0" style="border:0; margin-top:25px;" allowfullscreen></iframe> </h5>
+                        
+                        
+                    
+                    
+                    </div>
+                    
+                
+                
+                </div>
+                
+                <p class="copywrite"><em>&copy; Copywrite - Xmeagol - 2017</em></p>
+                            
+                            
+            </div>
+            
+           
+        
+            
+          
+        
+        
+     
+        
+        
+        
+        
+        
+        
+      
+      
+      
+    <!-- jQuery first, then Tether, then Bootstrap JS. -->
+    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+        
+        
+          <script type="text/javascript">
+            
+            
+       
+            
+        </script>
+        
+        
+
+        
+        
+        
+        
+        
+  </body>
+</html>
