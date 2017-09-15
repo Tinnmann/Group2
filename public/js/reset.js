ed53ba4bed2427714be689d26a5307bad10e9111
MAC2015 <mac2015@MAC2015s-MacBook-Pro.local>
2017-09-15 13:13:27 +0200

    js function reset

--- public/js/reset.js
+++ public/js/reset.js
@@ -1,3 +1,6 @@
-function clearField() {
-document.report-form.reset();
+function submitForm() {
+   var form = document.getElementsByName('report-form')[0];
+   form.submit(); // Submit the form
+   form.reset();  // Reset all form data
+   return false; // Prevent page refresh
 }
